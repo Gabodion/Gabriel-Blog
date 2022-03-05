105c334b4781b1a39127aa86fbc0315271c98774
Gabodion <gabodion@gmail.com>
2022-03-05 15:22:12 +0100

    update form.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 ##WTForm
@@ -14,7 +14,7 @@ class CreatePostForm(FlaskForm):
 # Register form
 class RegisterForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("SIGN ME UP!")
 
